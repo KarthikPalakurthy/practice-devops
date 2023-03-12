1cf3c64eff71a5a2acd76692d66a317ad59ca309
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-03-12 14:23:30 +0000

    shell scripting

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 cp /root/practice-devops/files/mongo.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y 
-sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.0/0.0.0.0' /etc/mongod.conf
 systemctl enable mongod 
 systemctl restart mongod 
